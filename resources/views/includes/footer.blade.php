382a7e12d049972a0f46ac41e70fd046e19428b6
kaksha96 <kdp55@njit.edu>
2018-10-15 16:57:08 -0400

    Task : Created footer.blame.php

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,7 @@
+
+<footer class="container">
+    <p class="float-right"><a hr="#">Return to Top</a></p>
+    <div  id="footer-copyright text-left">© Copyright 2018 Kaksha Patel Company
+    <a href="#">Terms and conditions</a>
+    </div>
+</footer>
