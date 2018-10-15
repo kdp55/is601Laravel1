1b5c79fc4f8f0aedd76bf0cdfdd1572bba16e8bd
kaksha96 <kdp55@njit.edu>
2018-10-15 17:27:44 -0400

    Task : Created about.blade.php

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.sidebar')
+@section('content')
+    <div class="container">
+<h2>About me</h2>
+<h3>I am 1st year NJIT student, Major Business and Information Systems.</h3>
+    </div>
+@stop
