2e166d87948faca1487f9666ce039ed80f4a5640
kaksha96 <kdp55@njit.edu>
2018-10-15 17:04:34 -0400

    Task : Created default.blame.php

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+    @include('includes.head')
+</head>
+<body>
+<div class="container">
+
+    <header class="row">
+        @include('includes.header')
+    </header>
+
+    <div id="main" class="row">
+
+        @yield('content')
+
+    </div>
+
+    <footer class="row">
+        @include('includes.footer')
+    </footer>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</div>
+</body>
+</html>
