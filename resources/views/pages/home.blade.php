7406621b9d53ebe4bda7e284af6f394c0cdec052
kaksha96 <kdp55@njit.edu>
2018-10-15 17:20:33 -0400

    Task : Created home.blame.php

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('content')
+   <div class="container">
+    <h1 class="font-color:Red">Welcome to My Home Page</h1>
+<h3 class="	glyphicon glyphicon-home">Sign up / Login</h3>
+   </div>
+@stop
