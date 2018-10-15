8c43ce1b5eb238736bf40c99d0f0517c60e85475
kaksha96 <kdp55@njit.edu>
2018-10-15 17:48:45 -0400

    task: made changes in header.php

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 
 <div class="navbar">
     <div class="navbar-inner">
-        <a id="logo" href="/">Single Malt</a>
+        <a id="logo" href="/">Welcome</a>
         <ul class="nav">
             <li><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
